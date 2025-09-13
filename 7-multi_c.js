372054035a9f203bf2eafdc13cd848de509225fe
Aloba busayo <alobabusayo01@gmail.com>
2025-09-14 00:10:16 +0100

    Create 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,5 @@
+Where x is the first argument of the script
+If the first argument can’t be converted to an integer,
+  print “Missing number of occurrences”
+If the first argument can’t be converted to an integer, 
+  print “C is fun”
