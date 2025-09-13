fb59216568f358fd660abcd4131df3ca31081192
Aloba busayo <alobabusayo01@gmail.com>
2025-09-14 00:12:59 +0100

    Create 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,5 @@
+The first argument is the size of the square
+If the first argument can’t be converted to an integer,
+  print “Missing size”
+If the first argument can be converted to an integer, 
+  print “XX”
