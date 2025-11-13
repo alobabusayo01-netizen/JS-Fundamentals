35b9876fc189042b0ca6606cdd7f6b83a7731a1d
Aloba busayo <alobabusayo01@gmail.com>
2025-11-13 16:34:27 +0100

    Refactor variable to array and update console logs

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,8 +1,3 @@
 
-const myVar = "C";
-console.log( myVar + "is fun");
-const myVar = "cool";
-console.log("Phython is " + myVar);
-const myVar = "amazing";
-console.log("javascript is " + myVar);
-
+const 'myVar' = ['C' , 'cool' , 'amazing'];
+console.log( [myVar + "is fun"] , ["Phython is " + myVar] , ["javascript is " + myVar]);
