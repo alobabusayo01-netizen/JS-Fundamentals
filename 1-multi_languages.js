bc05709bb95adf9dbce8039951bf6b80b5bd6678
Aloba busayo <alobabusayo01@gmail.com>
2025-11-13 16:04:17 +0100

    Refactor multi_languages.js to use variables

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,10 @@
-C is fun
-Python is cool
-Javascript is amazing
+
+const myVar = "C";
+console.log( myVar + "is fun");
+
+const myVar = "cool";
+console.log("Phython is " + myVar);
+
+const myVar = "amazing";
+console.log("javascript is " + myVar);
+
