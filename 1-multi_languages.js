9c71f8f262d9621cd0116ae3a3874dc3d5fd0f32
Aloba busayo <alobabusayo01@gmail.com>
2025-11-13 16:20:41 +0100

    Fix variable redeclaration in 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,10 +1,8 @@
 
 const myVar = "C";
 console.log( myVar + "is fun");
-
 const myVar = "cool";
 console.log("Phython is " + myVar);
-
 const myVar = "amazing";
 console.log("javascript is " + myVar);
 
