d6fd469fc16fb14155e262921db69919aca42db1
Aloba busayo <alobabusayo01@gmail.com>
2025-11-14 16:43:00 +0100

    Use a loop to print array elements
    Fix loop to correctly iterate over array elements.

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,1 +1,12 @@
-(like 1-multi_languages.js) but by using an array of string and a loop
+
+let a = ["C is fun" ,  "python is cool",  "javascript is amazing"];
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[3]);
+
+let i=a;
+
+while(i<=2){
+  console.log(i);
+  i++;
+}
