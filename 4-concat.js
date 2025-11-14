428b75a5473acf772088450fbb4f2c48efb320ba
Aloba busayo <alobabusayo01@gmail.com>
2025-11-14 16:15:59 +0100

    Refactor console.log statements for clarity

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,6 @@
 
 const myVar1  = X = "C";
 const myVar2 = Y = "undefined";
-console.log( [X + "cool"] , [ X +" is undefined " ] , ["undefined is " + Y]);
+console.log( X + " is cool" );
+console.log (  X +" is undefined " );
+console.log ("undefined is " + Y);
