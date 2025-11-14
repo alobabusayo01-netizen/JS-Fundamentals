ac21cbc18de33c706cbeb18bb847f0e8f91e474c
Aloba busayo <alobabusayo01@gmail.com>
2025-11-14 17:06:06 +0100

    Fix square size handling and output messages

--- 8-square.js
+++ 8-square.js
@@ -1,5 +1,8 @@
-The first argument is the size of the square
-If the first argument can’t be converted to an integer,
-  print “Missing size”
-If the first argument can be converted to an integer, 
-  print “XX”
+
+let arr = ["missing size", "xx" ];
+let i = x;
+while( i < arr.length){
+  console.log(arr[i]);
+  i++;
+}
+
