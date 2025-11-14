5b03607969585778a696c6e0de52a877557e05cf
Aloba busayo <alobabusayo01@gmail.com>
2025-11-14 17:17:08 +0100

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,3 +1,10 @@
-The first argument is the first integer
-The second argument is the second integer
-You have to define a function with this prototype: function add(a, b)
+
+function add(a,b) {
+  return a + b;
+}
+let result = add(0,1);
+console.log(result);
+let result = add(1,7);
+console.log(result);
+let result = add(13,89);
+console.log(result);
